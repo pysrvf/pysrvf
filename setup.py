a4b39f2741cd5b04b6020bee2e6becdb0164fc45
Shantanu H. Joshi <shjoshi@ieee.org>
2020-09-05 17:51:41 -0700

    setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysrvf',
+    version='0.1',
+    author='Elvis Nunez, Shantanu H. Joshi',
+    author_email='elvisnunez7@gmail.com, shjoshi@ieee.org',
+    description='pysrvf',
+    long_description='',
+    packages=['pysrvf'],
+    package_dir={'':'.'},
+    zip_safe=False
+)

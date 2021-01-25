Fixed scaling of Karcher mean

pysrvf/compute_geodesic.py
@@ -47,30 +47,30 @@ def compute_geodesic_C(q0, q1, stp, dt):
 	return alpha, alpha_t, E, L
 
 def compute_geodesic_B(q1, q2, stp):
-	'''
-	Computes geodesic from q0 to q1 (after project_B)
-	Inputs:
-	- q0: An (n x T) matrix representing SRVF curve
-	- q1: An (n x T) matrix representing SRVF curve
-	- stp: An integer specifying the number of points (minus 1) to sample on geodesic
-	- dt: Integer speciying step size
-	Outputs:
-	- alpha: An ((stp+1) x n x T) array representing the geodesic
-	- alpha_t: An ((stp+1) x n x T) array representing the geodesic derivative
-	- E: An array of max length 10 where each entry if the palais inner product
-	     for each alpha_t iteration 
-	- L: The length of the geodesic 
-	'''
-	theta = np.arccos(inner_product_L2(q1, q2))
-	f = q2 - inner_product_L2(q1,q2)*q1
-	f = theta*f/induced_norm_L2(f)
+  '''
+  Computes geodesic from q0 to q1 (after project_B)
+  Inputs:
+  - q0: An (n x T) matrix representing SRVF curve
+  - q1: An (n x T) matrix representing SRVF curve
+  - stp: An integer specifying the number of points (minus 1) to sample on geodesic
+  - dt: Integer speciying step size
+  Outputs:
+  - alpha: An ((stp+1) x n x T) array representing the geodesic
+  - alpha_t: An ((stp+1) x n x T) array representing the geodesic derivative
+  - E: An array of max length 10 where each entry if the palais inner product
+  for each alpha_t iteration 
+  - L: The length of the geodesic 
+  '''
+  theta = np.arccos(inner_product_L2(q1, q2))
+  f = (q2 - (inner_product_L2(q1,q2))*(q1))
+  f = theta * f/induced_norm_L2(f)
 
-	alpha = [project_B(geodesic_sphere(q1, f, tau/stp)) for tau in range(stp+1)]
-	alpha_t = dAlpha_dt(alpha, False)
-	E = palais_inner_product(alpha_t, alpha_t)
-	L = path_length(alpha_t)
+  alpha = [project_B(geodesic_sphere(q1, f, tau/stp)) for tau in range(stp+1)]
+  alpha_t = dAlpha_dt(alpha, False)
+  E = palais_inner_product(alpha_t, alpha_t)
+  L = path_length(alpha_t)
 
-	return alpha, alpha_t, E, L
+  return alpha, alpha_t, E, L
 
 def compute_geodesic_C_factor_D_closed(q1, q2, stp, d, dt):
 	'''
@@ -119,7 +119,7 @@ def compute_geodesic_C_factor_D_closed(q1, q2, stp, d, dt):
 		E_geo_C.append(E[-1])
 		u = alpha_t[stp]
 		D_q = form_basis_D_q(V, q2n)
-		u_proj, a = project_tgt_D_q(u, D_q) # Need to check this
+		u_proj, a = project_tgt_D_q(u, D_q)
 		g = np.matmul(a, V)
 		A_norm_iter.append(trapz(np.square(g), s))
 
@@ -145,79 +145,77 @@ def compute_geodesic_C_factor_D_closed(q1, q2, stp, d, dt):
 	return alpha, alpha_t, A_norm_iter, E_geo_C, gamma, geo_dist
 
 def compute_geodesic_C_factor_D_open(q1, q2, stp, d, dt):
-	'''
-	Computes geodesic from q0 to q1 and warping function (for open curves)
-	Inputs:
-	- q0: An (n x T) matrix representing SRVF curve
-	- q1: An (n x T) matrix representing SRVF curve
-	- stp: An integer specifying the number of points (minus 1) to sample on geodesic
-	- d: The number of Fourier coefficients (divided by 2)
-	- dt: Integer speciying step size
-	Outputs:
-	- alpha: An ((stp+1) x n x T) array representing the geodesic
-	- alpha_t: An ((stp+1) x n x T) array representing the geodesic derivative
-	- A_norm_iter: An array of max length 45 ...
-	- E_geo_C: An array of max length 45 containint palais inner products
-	- gamma: A T-dimensional vector representing the warping of q2 to q1
-	- geo_dist: The length of the geodesic
-	'''
-	itr = 0 
-	n, T = np.shape(q1)
-	V = form_basis_D(d, T)
-	s = np.linspace(0, 2*np.pi, T, True)
-	epsilon = 1e-6
-	diffL = 100
+  '''
+  Computes geodesic from q0 to q1 and warping function (for open curves)
+  Inputs:
+  - q0: An (n x T) matrix representing SRVF curve
+  - q1: An (n x T) matrix representing SRVF curve
+  - stp: An integer specifying the number of points (minus 1) to sample on geodesic
+  - d: The number of Fourier coefficients (divided by 2)
+  - dt: Integer speciying step size
+  Outputs:
+  - alpha: An ((stp+1) x n x T) array representing the geodesic
+  - alpha_t: An ((stp+1) x n x T) array representing the geodesic derivative
+  - A_norm_iter: An array of max length 45 ...
+  - E_geo_C: An array of max length 45 containint palais inner products
+  - gamma: A T-dimensional vector representing the warping of q2 to q1
+  - geo_dist: The length of the geodesic
+  '''
 
-	q1 = project_B(q1)
-	q2n = project_B(q2)
-	q2n, gamma = initialize_gamma_using_DP(q1, q2n, False)
-	q2n = project_B(q2n)
-	alpha, alpha_T, E, L = compute_geodesic_B(q1, q2n, stp)
+  itr = 0 
+  n, T = np.shape(q1)
+  V = form_basis_D(d, T)
+  s = np.linspace(0, 2*np.pi, T, True)
+  epsilon = 1e-6
+  diffL = 100
 
+  q1 = project_B(q1)
+  q2n = project_B(q2)
+  q2n, gamma = initialize_gamma_using_DP(q1, q2n, False)
+  q2n = project_B(q2n)
+  alpha, alpha_T, E, L = compute_geodesic_B(q1, q2n, stp)
 
-	if (induced_norm_L2(q1 - q2n)**2) < epsilon/15:
-		A_norm_iter = 0
-		E_geo_C = 0
-		gamma = s
-		geo_dist = 0
-		alpha = np.zeros((stp, n, T))
-		alpha = [q1 for i in range(stp)]
-		alpha_t = np.zeros((stp+1, n, T))
-		return alpha, alpha_t, A_norm_iter, E_geo_C, gamma, geo_dist
+  if (induced_norm_L2(q1 - q2n)**2) < epsilon/15:
+    A_norm_iter = 0
+    E_geo_C = 0
+    gamma = s
+    geo_dist = 0
+    alpha = np.zeros((stp, n, T))
+    alpha = [q1 for i in range(stp)]
+    alpha_t = np.zeros((stp+1, n, T))
+    return alpha, alpha_t, A_norm_iter, E_geo_C, gamma, geo_dist
 
+  E_geo_C = []
+  A_norm_iter = []
+  L_iter = []
+  while (itr < 44) and (diffL > epsilon/100):
+    q2n = project_B(q2n)
+    alpha, alpha_t, E, L = compute_geodesic_B(q1, q2n, stp)
+    L_iter.append(L)
+    E_geo_C.append(E)
+    u = alpha_t[stp]
+    D_q = form_basis_D_q(V, q2n)
+    u_proj, a = project_tgt_D_q(u, D_q)
+    g = np.matmul(a, V)
+    A_norm_iter.append(trapz(np.square(g), s))
 
-	E_geo_C = []
-	A_norm_iter = []
-	L_iter = []
-	while (itr < 44) and (diffL > epsilon/100):
-		q2n = project_B(q2n)
-		alpha, alpha_t, E, L = compute_geodesic_B(q1, q2n, stp)
-		L_iter.append(L)
-		E_geo_C.append(E)
-		u = alpha_t[stp]
-		D_q = form_basis_D_q(V, q2n)
-		u_proj, a = project_tgt_D_q(u, D_q) 
-		g = np.matmul(a, V)
-		A_norm_iter.append(trapz(np.square(g), s))
+    # Form gamma
+    gamma_n = s - epsilon*g
+    gamma_n = gamma_n - gamma_n[0]
+    # gamma_n = 2*np.pi*gamma_n/np.max(gamma_n)
+    if np.sum(gamma_n < 0) or np.sum(np.diff(gamma_n) < 0):
+      warnings.warn('Gamma is invalid')
+      break 
 
-		# Form gamma
-		gamma_n = s - epsilon*g 
-		gamma_n = gamma_n - gamma_n[0]
-		# gamma_n = 2*np.pi*gamma_n/np.max(gamma_n)
+    q2n = group_action_by_gamma(q2n, gamma_n, False)
+    q2n = project_B(q2n)
 
-		if np.sum(gamma_n < 0) or np.sum(np.diff(gamma_n) < 0):
-			warnings.warn('Gamma is invalid')
-			break 
-
-		q2n = group_action_by_gamma(q2n, gamma_n, False)
-		q2n = project_B(q2n)
-
-		itr += 1
-		if itr > 1:
-			diffL = np.linalg.norm(L_iter[itr-1] - L_iter[itr-2])
-	gamma = estimate_gamma(q2n, False)
-	geo_dist = L
-	return alpha, alpha_t, A_norm_iter, E_geo_C, gamma, geo_dist
+    itr += 1
+    if itr > 1:
+      diffL = np.linalg.norm(L_iter[itr-1] - L_iter[itr-2])
+  gamma = estimate_gamma(q2n, False)
+  geo_dist = L
+  return alpha, alpha_t, A_norm_iter, E_geo_C, gamma, geo_dist
 
 def compute_geodesic_C_factor_D_symm(q1, q2, stp, d, dt):
 	'''
@@ -246,7 +244,7 @@ def compute_geodesic_C_factor_D_symm(q1, q2, stp, d, dt):
 
 	q1 = project_B(q1)
 	q2n = project_B(q2)
-	q2n, _ = initialize_gamma_using_DP_symm(q1, q2, False)
+	q2n = initialize_gamma_using_DP_symm(q1, q2, False)
 	q2n = project_B(q2n)
 	alpha, alpha_T, E, L = compute_geodesic_B(q1, q2n, stp)
 

pysrvf/form_basis_utils.py
@@ -4,7 +4,7 @@ from pysrvf.generic_utils import *
 
 
 def form_basis_D(d, T):
-	''' 
+  ''' 
 	Returns the basis for the tangent space of diffeomorphisms
 	Inputs:
 	- d: A nonnegative integer (number of Fourier coefficients divided by 2)
@@ -12,20 +12,23 @@ def form_basis_D(d, T):
 	Ouputs:
 	- V: A (2d x T) matrix representing the basis
 	'''
-	x = np.reshape(np.linspace(0, 2*np.pi, T, True), (1, T))
-	vec = np.reshape(np.arange(1, d+1), (1, d))
-	x_d_arr = np.matmul(vec.T, x)
-	V = np.vstack([np.cos(x_d_arr)/np.sqrt(np.pi), np.sin(x_d_arr)/np.sqrt(np.pi)])
-	return V
+  x = np.linspace(0, 2*np.pi, T)
+  xdarray = np.arange(1,d+1) 
+  xdarray = np.outer(xdarray,x)
+  V_cos = np.cos(xdarray) / np.sqrt(np.pi)
+  V_sin = np.sin(xdarray) / np.sqrt(np.pi)
+  V = np.concatenate((V_cos,V_sin))
+  x = np.reshape(np.linspace(0, 2*np.pi, T, True), (1, T))
+  return V
 
 def form_basis_D_q(V, q):
 	''' 
 	Returns ... 
 	Inputs:
-	- V: A (d x T) matrix representing basis for tangent space of diffeomorphisms
+	- V: A ((2*d) x T) matrix representing basis for tangent space of diffeomorphisms
 	- q: An (n x T) matrix representation of the SRVF q: [0,2pi] -> R^n
 	Ouputs:
-	- D_q: A (d x n x T) array
+	- D_q: A ((2*d) x n x T) array
 	'''
 	d, _ = np.shape(V)
 	n, T = np.shape(q)
@@ -47,10 +50,10 @@ def project_tgt_D_q(u, D_q):
 	Returns ... 
 	Inputs:
 	- u: An (n x T) matrix
-	- D_q: A (d x n x T) array
+	- D_q: A ((2*d) x n x T) array
 	Ouputs:
 	- u_proj: An (n x T) matrix of projection of u 
-	- a: A d-dimensional vector representing Fourier coefficients
+	- a: A (1 x (2*d)) vector representing Fourier coefficients
 	'''
 	d, n, T = np.shape(D_q)
 
@@ -128,33 +131,47 @@ def project_tangent(f, q, is_closed):
 
 
 def form_basis_L2_R3(d, T):
+  '''
+  Returns Schauder basis for L_2(R^3)
+  Note basis elements will be 6 x d
+  '''
 
-	x = np.linspace(0, 2 * np.pi, T, True)
-	k = 0
-	sqrt_2 = np.sqrt(2);
-	constB = np.zeros((3, 3, T))
+  x = np.linspace(0, 2 * np.pi, T, True)
+  sqrt_2 = np.sqrt(2)
+  constB = np.zeros((3, 3, T))
 
-	constB[0] = np.array([np.sqrt(2) * np.ones(T), np.zeros(T), np.zeros(T)])
-	constB[1] = np.array([np.zeros(T), np.sqrt(2) * np.ones(T), np.zeros(T)])
-	constB[2] = np.array([np.zeros(T), np.zeros(T), np.sqrt(2) * np.ones(T)])
+  constB[0] = np.array([np.sqrt(2) * np.ones(T), np.zeros(T), np.zeros(T)])
+  constB[1] = np.array([np.zeros(T), np.sqrt(2) * np.ones(T), np.zeros(T)])
+  constB[2] = np.array([np.zeros(T), np.zeros(T), np.sqrt(2) * np.ones(T)])
 
-	B = np.zeros( (6*d, 3, T))
-	for j in np.arange(1, d+1):
-		B[0 + 6*k] = np.array([np.sqrt(2) * np.cos(2 * np.pi * j * x), np.zeros(T), np.zeros(T)])
-		B[1 + 6*k] = np.array([np.zeros(T), np.sqrt(2) * np.cos(2 * np.pi * j * x), np.zeros(T)])
-		B[2 + 6*k] = np.array([np.zeros(T), np.zeros(T), np.sqrt(2) * np.cos(2 * np.pi * j * x)])
-		B[3 + 6*k] = np.array([np.sqrt(2) * np.sin(2 * np.pi * j * x), np.zeros(T), np.zeros(T)])
-		B[4 + 6*k] = np.array([np.zeros(T), np.sqrt(2) * np.sin(2 * np.pi * j * x), np.zeros(T)])
-		B[5 + 6*k] = np.array([np.zeros(T), np.zeros(T), np.sqrt(2) * np.sin(2 * np.pi * j * x)])
-		k = k + 1
+  B = np.zeros((6*d, 3, T))
+  k = 0
+  for j in np.arange(1, d+1):
+    B[0 + 6*k] = np.array([np.sqrt(2) * np.cos(2 * np.pi * j * x), np.zeros(T), np.zeros(T)])
+    B[1 + 6*k] = np.array([np.zeros(T), np.sqrt(2) * np.cos(2 * np.pi * j * x), np.zeros(T)])
+    B[2 + 6*k] = np.array([np.zeros(T), np.zeros(T), np.sqrt(2) * np.cos(2 * np.pi * j * x)])
+    B[3 + 6*k] = np.array([np.sqrt(2) * np.sin(2 * np.pi * j * x), np.zeros(T), np.zeros(T)])
+    B[4 + 6*k] = np.array([np.zeros(T), np.sqrt(2) * np.sin(2 * np.pi * j * x), np.zeros(T)])
+    B[5 + 6*k] = np.array([np.zeros(T), np.zeros(T), np.sqrt(2) * np.sin(2 * np.pi * j * x)])
+    k = k + 1
 
-	B = np.concatenate((constB, B))
+  #B[0] = B[0] * constB[0]
+  #B_tile = np.tile(constB,(1,4))
+  #print(B_tile.shape)
+  #print(B_tile)
 
-	return B
+  #B = np.concatenate((constB, B))
+  #B = np.append((constB, B), axis = 1)
+  #Return to this, the shape is correct, but the values look off
+
+  return B
+
 
 def form_basis_of_tangent_space_of_S_at_q(Bnew, G_O_q):
 
 	# T_q(S) = T_q(C) + T_q(O_q)^{\perp}
+  # S in this case references closed orbits of C^o
+  # S = {[q] | q in C^o}
 	# Subtract the projection of basis of T_q(C) onto T_q(O_q) from itself
 	# i.e. basis(T_q(C)) - <basis(T_q(C)), basis(T_q(O_q))> * basis(T_q(O_q))
 
@@ -184,7 +201,7 @@ def project_to_basis(alpha_t_array, Y):
 
 	return A, V
 
-
+# Needs editing
 def form_basis_O_q(B,q):
     d = len(B)
     n,T = q.shape

pysrvf/generic_utils.py
@@ -55,63 +55,65 @@ def form_basis_normal_A(q):
 	return del_g
 
 def projectC(q):
-    ''' 
-    Projects the given SRVF curve onto the space of closed curves
-    Inputs:
-    - q: An (n x T) matrix representing the SRVF representation of a curve
-    Outputs:
-    - qnew: An (n x T) matrix representing the SRVF of a closed curve
-    '''
-    n, T = np.shape(q)
-    dt = 0.3
-    epsilon = (1.0/60.0)*(2.0*np.pi/T)
+  ''' 
+  Projects the given SRVF curve onto the space of closed curves
+  Inputs:
+  - q: An (n x T) matrix representing the SRVF representation of a curve
+  Outputs:
+  - qnew: An (n x T) matrix representing the SRVF of a closed curve
+  '''
+  n, T = np.shape(q)
+  dt = 0.3
+  epsilon = (1.0/60.0)*(2.0*np.pi/T)
 
-    k = 0
-    res = np.ones((1,n))
-    J = np.zeros((n, n))
-    
-    eps = np.spacing(1)
-    qnew = q/(induced_norm_L2(q) + eps)
+  k = 0
+  res = np.ones((1,n))
+  J = np.zeros((n, n))
+  
+  eps = np.spacing(1)
+  qnew = q/(induced_norm_L2(q) + eps)
 
-    while(np.linalg.norm(res, 2) > epsilon):
-        if k > 300:
-            warnings.warn('Shape failed to project. Geodesics will be incorrect.')
-            break
+  while(np.linalg.norm(res, 2) > epsilon):
+      if k > 300:
+          warnings.warn('Shape failed to project. Geodesics will be incorrect.')
+          break
 
-        # Compute Jacobian
-        for i in range(n):
-            for j in range(n):
-                J[i, j] = 3*np.trapz(np.multiply(qnew[i, :], qnew[j, :]), dx = 2*np.pi/(T-1))
-        J += np.eye(n)
+      # Compute Jacobian
+      for i in range(n):
+          for j in range(n):
+              J[i, j] = 3*np.trapz(np.multiply(qnew[i, :], qnew[j, :]), dx = 2*np.pi/(T-1))
+      J += np.eye(n)
 
-        qnorm = np.linalg.norm(qnew, 2, axis = 0)
+      qnorm = np.linalg.norm(qnew, 2, axis = 0)
 
-        # Compute residue
-        res = [-np.trapz(np.multiply(qnew[i,:], qnorm), dx = 2*np.pi/(T-1)) for i in range(n)]
+      # Compute residue
+      res = [-np.trapz(np.multiply(qnew[i,:], qnorm), dx = 2*np.pi/(T-1)) for i in range(n)]
 
-        if np.linalg.norm(res, 2) < epsilon:
-            break
+      if np.linalg.norm(res, 2) < epsilon:
+          break
 
-        J_cond = np.linalg.cond(J)
+      J_cond = np.linalg.cond(J)
 
-        if np.isnan(J_cond) or np.isinf(J_cond) or (J_cond < 0.1):
-            warnings.warn('Projection may not be accurate.')
-            return q/(induced_norm_L2(q) + eps)
-        else:
-            x = np.linalg.solve(J, res)
-            del_G = form_basis_normal_A(qnew)
-            temp = 0
-            for i in range(n):
-                temp += x[i]*del_G[i]*dt
-            qnew += temp
-            k += 1
+      if np.isnan(J_cond) or np.isinf(J_cond) or (J_cond < 0.1):
+          warnings.warn('Projection may not be accurate.')
+          return q/(induced_norm_L2(q) + eps)
+      else:
+          x = np.linalg.solve(J, res)
+          del_G = form_basis_normal_A(qnew)
+          temp = 0
+          for i in range(n):
+              temp += x[i]*del_G[i]*dt
+          qnew += temp
+          k += 1
 
-    qnew = qnew/induced_norm_L2(qnew)
-    
-    return qnew
+  induced_norm = induced_norm_L2(qnew)
+
+  qnew = qnew/induced_norm
+
+  return qnew
 
 def project_B(q):
-	'''
+  '''
 	Projects given point in L2 onto unit Hilbert sphere in L2
 	Inputs: 
 	- q: An (n x T) matrix representation of the SRVF of function p: D -> R^n
@@ -120,9 +122,9 @@ def project_B(q):
 	Outputs:
 	An (n x T) matrix representation of q projected on the Hilbert sphere
 	'''
-	
-	return q/induced_norm_L2(q) 
-	
+  induced_norm = induced_norm_L2(q)
+  return q/induced_norm
+
 def curve_to_q(p, closed = False, shape = None):
 	''' 
 	Given a curve p, gets the SRVF representation
@@ -229,7 +231,7 @@ def batch_q_to_curve(srvfs):
 	else:
 		print('Open srvfs detected. {} total curves.'.format(N))
 
-        #Make sure to add additional parameters to match curve_to_q
+        #Make sure to add additional parameters to match curve_to_q ???
 	return [q_to_curve(q_i) for q_i in srvfs], is_closed
 	
 def reparameterize_curve_gamma(curve, gamma):